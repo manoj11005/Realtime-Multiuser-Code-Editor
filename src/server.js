1758271c035c4369bbbf96a899e2a9c1c5f23de4
Manoj Kumar <2023eeb1223@iitrpr.ac.in>
2026-06-16 14:50:05 +0530

    fix refresh issue

--- src/server.js
+++ src/server.js
@@ -1,76 +1,77 @@
-const express=require('express')
-const app=express();
-const http=require('http');
-const path=require('path');
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
 const ACTIONS = require('./Actions');
-const {Server}=require('socket.io');
-const server=http.createServer(app);
-const io=new Server(server);
-app.use(express.static('build'));
-app.use((req,res,next)=>{
-    res.sendFile(path.join(__dirname,'builld','index.html'))
+const { Server } = require('socket.io');
 
-})
-const userSocketMap={};
+const server = http.createServer(app);
+const io = new Server(server);
 
-function getAllConnectedClients(roomId){
-    // map
+// Serve React build
+app.use(express.static(path.join(__dirname, '../build')));
+
+// React Router refresh fix
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
+const userSocketMap = {};
+
+function getAllConnectedClients(roomId) {
     return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
-        (socketId)=>{
-            return{
+        (socketId) => {
+            return {
                 socketId,
-                username:userSocketMap[socketId],
+                username: userSocketMap[socketId],
             };
         }
     );
 }
 
+io.on('connection', (socket) => {
+    console.log('socket connected', socket.id);
 
-io.on('connection',(socket)=>{
-    console.log('socket connected',socket.id);
-    socket.on(ACTIONS.JOIN,({roomId,username})=>{
-        userSocketMap[socket.id]=username;
+    socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
+        userSocketMap[socket.id] = username;
         socket.join(roomId);
-        const clients=getAllConnectedClients(roomId);
-        clients.forEach(({socketId})=>{
-            io.to(socketId).emit(ACTIONS.JOINED,{
+
+        const clients = getAllConnectedClients(roomId);
+
+        clients.forEach(({ socketId }) => {
+            io.to(socketId).emit(ACTIONS.JOINED, {
                 clients,
                 username,
-                socketId:socket.id,
-
-            });
-
-        });
-
-    });
-
-
-    socket.on(ACTIONS.CODE_CHANGE,({roomId,code})=>{
-        socket.in(roomId).emit(ACTIONS.CODE_CHANGE,{code});
-
-
-    
-    });
-    socket.on(ACTIONS.SYNC_CODE,({socketId,code})=>{
-        io.to(socketId).emit(ACTIONS.CODE_CHANGE,{code});
-
-
-    
-    });
-
-    socket.on('disconnecting',()=>{
-        const rooms=[...socket.rooms];
-        rooms.forEach((roomId)=>{
-            socket.in(roomId).emit(ACTIONS.DISCONNECTED,{
-                socketId:socket.id,
-                username:userSocketMap[socket.id],
+                socketId: socket.id,
             });
         });
+    });
+
+    socket.on(ACTIONS.CODE_CHANGE, ({ roomId, code }) => {
+        socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code });
+    });
+
+    socket.on(ACTIONS.SYNC_CODE, ({ socketId, code }) => {
+        io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });
+    });
+
+    socket.on('disconnecting', () => {
+        const rooms = [...socket.rooms];
+
+        rooms.forEach((roomId) => {
+            socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
+                socketId: socket.id,
+                username: userSocketMap[socket.id],
+            });
+        });
+
         delete userSocketMap[socket.id];
         socket.leave();
-    })
+    });
 });
 
+const PORT = process.env.PORT || 5000;
 
-const PORT=process.env.PORT || 5000;
-server.listen(PORT,()=>console.log(`Listening on port ${PORT}`));
+server.listen(PORT, () =>
+    console.log(`Listening on port ${PORT}`)
+);
