a47add3df1041714f2f71f4a38f7a4600c423c91
Manoj Kumar <2023eeb1223@iitrpr.ac.in>
2026-06-16 15:58:41 +0530

    fix refresh issue

--- src/server.js
+++ src/server.js
@@ -9,9 +9,11 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 // Serve React build
+// Serve React build
 app.use(express.static(path.join(__dirname, '../build')));
 
-app.use((req, res) => {
+// React Router refresh fix
+app.get('/{*any}', (req, res) => {
     res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
