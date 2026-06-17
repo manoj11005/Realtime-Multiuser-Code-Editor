1d489e642e855d7876f5ce256f7104f74dfa4824
Manoj Kumar <2023eeb1223@iitrpr.ac.in>
2026-06-18 00:19:20 +0530

    Fix socket url

--- src/socket.js
+++ src/socket.js
@@ -1,11 +1,22 @@
-import {io} from 'socket.io-client';
-export const initSocket=async()=>{
-    const options={
-        'force new connection':true,
-        reconnectionAttempt:'Infinity',
-        timeout:10000,
-        transports:['websocket'],
+// import {io} from 'socket.io-client';
+// export const initSocket=async()=>{
+//     const options={
+//         'force new connection':true,
+//         reconnectionAttempt:'Infinity',
+//         timeout:10000,
+//         transports:['websocket'],
 
-    };
-    return io(process.env.REACT_APP_BACKEND_URL,options);
+//     };
+//     return io(process.env.REACT_APP_BACKEND_URL,options);
+// };
+
+
+
+import { io } from 'socket.io-client';
+
+export const initSocket = async () => {
+    return io(window.location.origin, {
+        transports: ['websocket'],
+        timeout: 10000,
+    });
 };
